d30c4d21b411a2856abbb46f566f6b3afe633341
Fard <fardilizer@gmail.com>
2025-11-20 21:59:18 -0800

    update

---
+++ src/main/java/com/fardilizer/createknickknacks/item/ModItems.java
@@ -0,0 +1,14 @@
+package com.fardilizer.createknickknacks.item;
+
+import com.fardilizer.createknickknacks.createknickknacks;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(createknickknacks.MOD_ID);
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
